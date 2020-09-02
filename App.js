33e5ec385349459758e9b2bd5d7d02aa48ae9ea5
Chris Van Huffe <cvanhuffel@outlook.com>
2020-09-02 13:37:39 -0700

    Emu Tour

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text1}>What Up Cats!?</Text>
+      <Text style={styles.text2}>Koa Kitty Rules!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +15,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text1: {
+    fontWeight: 'bold',
+    fontSize: 50,
+    color: 'purple',
+  },
+  text2: {
+    fontWeight: 'bold',
+    fontSize: 30,
+    color: 'darkblue',
+  }
 });
