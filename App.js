295b33f8b5ac752d55cc9948f5eef9dbb2cfce8c
Chris Van Huffe <cvanhuffel@outlook.com>
2020-09-06 13:13:46 -0700

    First Compenents

--- App.js
+++ App.js
@@ -1,32 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './components/MainComponent';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text1}>What Up Cats!?</Text>
-      <Text style={styles.text2}>Koa Kitty Rules!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    return (
+        <Main />
+    );
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'lightblue',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text1: {
-    fontWeight: 'bold',
-    fontSize: 50,
-    color: 'purple',
-  },
-  text2: {
-    fontWeight: 'bold',
-    fontSize: 30,
-    color: 'darkblue',
-  }
-});
