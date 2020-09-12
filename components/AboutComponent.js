eca34b9a33b6999bb721dee93d7b938decc85b58
Chris Van Huffe <cvanhuffel@outlook.com>
2020-09-12 11:28:51 -0700

    Week 1 final

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView } from 'react-native';
-import { Card } from 'react-native-elements';
+import { View, Text, ScrollView, FlatList } from 'react-native';
+import { Card, ListItem } from 'react-native-elements';
 import { PARTNERS } from '../shared/partners';
 
-
+function Mission() {
+  return (
+    <Card title={'Our Mission'} wrapperStyle={{margin: 10}} >
+      <Text>
+        We present a curated database of the best campsites in the vast woods and   backcountry of the World Wide Web Wilderness. We increase access to adventure   for the public while promoting safe and respectful use of resources. The expert   wilderness trekkers on our staff personally verify each campsite to make sure   that they are up to our standards. We also present a platform for campers to  share reviews on campsites they have visited with each other.
+      </Text>
+    </Card>
+  )
+}
 
 
 class About extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      //partners: PARTNERS
+    partners: PARTNERS
     };
   }
 
@@ -19,11 +27,28 @@ class About extends Component {
   }
 
   render() {
-    return (
-    <ScrollView>
-      
-    </ScrollView>
-    );
+    const { navigate } = this.props.navigation;
+      const renderPartner = ({item}) => {
+        return (
+          <ListItem
+            title={item.name}
+            subtitle={item.description}
+            leftAvatar={{ source: require('./images/bootstrap-logo.png')}}
+          />
+        );
+      }
+        return (
+        <ScrollView>
+          <Mission/>
+          <Card title={'Community Partners'} wrapperStyle={{margin: 10}}>
+            <FlatList
+              data={this.state.partners}
+              renderItem={renderPartner}
+              keyExtractor={item => item.id.toString()}
+            />
+          </Card>
+        </ScrollView>
+        );
   }
 }
 
--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -20,10 +20,18 @@ class Contact extends Component {
   render() {
     return (
     <ScrollView>
-
+      <Card
+      title={'Contact Information'}
+      wrapperStyle={{margin: 20}}>
+        <Text>1 Nucamp Way</Text>
+        <Text>Seattle, WA 98001</Text>
+        <Text style={{marginBottom: 10}}>USA</Text>
+        <Text >Phone: 1-206-555-1234</Text>
+        <Text>Email: campsites@nucamp.co</Text>
+      </Card>
     </ScrollView>
     );
   }
 }
 
-export default Contact
+export default Contact;
