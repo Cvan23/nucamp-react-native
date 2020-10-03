c914cd6213a75680feff43486aed6c1110b253b9
Chris Van Huffe <cvanhuffel@outlook.com>
2020-10-03 12:26:57 -0700

    Week 4 final

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -156,18 +156,35 @@ class RegisterTab extends Component {
             });
             if (!capturedImage.cancelled) {
                 console.log(capturedImage);
-                this.setState({imageUrl: capturedImage.uri});
+                this.processImage(capturedImage.uri);
             }
         }
     }
 
     processImage = async (imgUri) => {
         const processedImage = await ImageManipulator.manipulateAsync(
-            image.localUri || image.uri,
-            [{ rotate: 90 }, { flip: ImageManipulator.FlipType.Vertical }],
+            imgUri,
+            [{resize:{width:400}}],
             { compress: 1, format: ImageManipulator.SaveFormat.PNG }
           );
-          setImage(processedImage);
+          console.log(processedImage);
+          this.setState({imageUrl: processedImage.uri});
+    }
+
+    getImageFromGallery = async () => {
+        const cameraRollPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+        if (cameraRollPermission.status === 'granted' ) {
+            const capturedImage = await  ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [1, 1]
+            });
+            if (!capturedImage.cancelled) {
+                console.log(capturedImage);
+                this.processImage(capturedImage.uri);
+            }
+        }
+
     }
 
     handleRegister() {
@@ -196,7 +213,12 @@ class RegisterTab extends Component {
                             title='Camera'
                             onPress={this.getImageFromCamera}
                         />
+                        <Button
+                            title='Gallery'
+                            onPress={this.getImageFromGallery}
+                        />
                     </View>
+                    
                     <Input
                         placeholder='Username'
                         leftIcon={{type: 'font-awesome', name: 'user-o'}}
